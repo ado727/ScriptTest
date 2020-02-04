7e3617d8e3ff878374b6dc5c2a9e06e74c8ae0fb
morikazu-toyonaga <blackadotan3@gmail.com>
2020-02-04 19:20:11 +0900

    再提出

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,24 +4,26 @@ using System.Collections;
 public class Test : MonoBehaviour {
 
 	void Start () {
-
+		
 		int[] array = new int[5];
 
 		array [0] = 10;
-		array [1] = 90;
-		array [2] = 90;
-		array [3] = 70;
-		array [4] = 20;
+		array [1] = 20;
+		array [2] = 30;
+		array [3] = 40;
+		array [4] = 50;
 
 		for (int a = 0; a < 5; a++) {
 			Debug.Log (array [a]);
 		}
-		for (int b = 4; b < 5; b--){
+
+
+		for (int b = 4; b >= 0; b--){
 			Debug.Log (array [b]);
-		}
-
+		} 
+			
 	}
-
+		
 	void Update () {
 
 	}
